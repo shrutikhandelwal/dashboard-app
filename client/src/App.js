a25835fa1271c0a7f9f6e55e2751ce35de26c840
Shruti Khandelwal <kh.shruti88@gmail.com>
2024-05-10 11:11:27 -0700

    Working Customer's page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import { themeSettings } from "theme";
 import { Dashboard } from "scenes/dashboard";
 import { Layout } from 'scenes/layout';
 import {Products} from "scenes/Products" 
+import {Customers} from "scenes/Customers"
+
 
 
 function App() {
@@ -22,6 +24,7 @@ function App() {
                 <Route path= "/" element={<Navigate to="/dashboard" replace />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/customers" element={<Customers />} />
               </Route>
             </Routes>
         </ThemeProvider>
