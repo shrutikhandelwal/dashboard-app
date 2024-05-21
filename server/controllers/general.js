0e51abe41046f0dfbbac6a62617e0a0c19c86ce0
Shruti Khandelwal <kh.shruti88@gmail.com>
2024-05-21 15:59:36 -0700

    Final push

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,4 @@
-import User from "../models/user.js"
+import User from "../models/User.js"
 import OverallStat from "../models/OverallStat.js";
 import Transaction from "../models/Transaction.js";
 
